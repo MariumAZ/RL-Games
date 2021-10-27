1449d9831102ae4bc7f6a76f45e976021e2a42e7
MariumAZ <mariem.azouz@dauphine.tn>
2021-10-27 11:18:01 +0100

    train mountain_car

--- Mountaincar/train.py
+++ Mountaincar/train.py
@@ -2,12 +2,18 @@
 
 import gym
 import numpy as np
+import utility
 
-episodes = 20000
+episodes = 250000
+#discount rate : how much value we give to the future 
 gamma = 0.9
-alpha = 0.1 #learning rate
-max_steps = 1000
+#learning rate
+alpha = 0.1 
+max_steps = 100
 epsilon = 0.98
+init_epsilon = epsilon
+min_epsilon = 0.1
+epsilon_decay = 0.05
 
 #initialize Q table:
 
@@ -15,31 +21,46 @@ epsilon = 0.98
 env = gym.make("MountainCar-v0")
 print(env.action_space)
 print(env.observation_space)
+print(env.goal_position)
 
-
-def pick_action(epsilon, Q, state):
-    if np.random.uniform() < epsilon :
-        action = np.random.randint(env.actions_space.n)
-    else: #pick the max action
-        action = np.argmax(Q[state])
-    return action    
-
-q_shape = tuple([env.observation, env.action_space.n])
-Q = np.zeros(shape = q_shape)
-for _ in range(episodes):
+total_rewards = []
+rewards_per_episode = []
+q_shape = [utility.n_states, utility.n_states,  utility.n_actions]
+#Q = np.zeros(shape = tuple(q_shape))
+Q = np.random.uniform(low=-1, high=1, size= tuple(q_shape))
+for episode in range(episodes):
     state = env.reset()
-    step = 0 
-    action = pick_action(epsilon, Q, state)
-    while step < max_steps:
+    state = utility.discretize(state)
+    done = False
+    while not done:
+        #pick an action
+        action = utility.pick_action(epsilon, Q, state)
         env.render()
         new_state, reward, done, info = env.step(action)
+        new_state = utility.discretize(new_state)
         q_current = Q[state][action]
+        #print("q_current", q_current)
         q_target = np.max(Q[new_state])
         q_current = (1 - alpha) * q_current + alpha * (reward + gamma * q_target)
         state = new_state
+        print(state)
+        if new_state[0] >= env.goal_position :
+            reward = 1
+        rewards_per_episode.append(reward)
         if done:
-            break
-        step += 1
+            break    
+    total_rewards.append(rewards_per_episode)    
+    #reduce epsilon decay
+    epsilon = (min_epsilon + (init_epsilon - min_epsilon) * np.exp(-epsilon_decay * episode))
+    #print(Q)
+np.save("Mountaincar_qtable", Q)
+env.close()
+        
+
+
+
+
+
 
 
 
--- Mountaincar/utility.py
+++ Mountaincar/utility.py
@@ -9,6 +9,7 @@ low_values = np.array([-1.2, -0.7])
 high_values = np.array([0.6, 0.7])
 
 n_states = 40
+n_actions = 3  
 distance = (high_values - low_values) 
 step = distance / n_states
 
@@ -23,6 +24,15 @@ def discretize(state):
 
     state = (state - low_values) / step
     return tuple(state.astype(int))
+
+def pick_action(epsilon, Q, state):
+    if np.random.uniform() < epsilon :
+          #explore 
+        action = np.random.randint(n_actions)
+    else: #pick the max action
+        action = np.argmax(Q[state])
+    return action    
+
 
 
 
