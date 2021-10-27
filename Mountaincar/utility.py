2cd688859c28b4da5a3df07bbcd877eeff60246a
MariumAZ <mariem.azouz@dauphine.tn>
2021-10-27 09:02:07 +0100

    added discretise function

--- Mountaincar/utility.py
+++ Mountaincar/utility.py
@@ -1,2 +1,22 @@
 
+import numpy as np
+
+low_values = np.array([-1.2, -0.7])
+high_values = np.array([0.6, 0.7])
+
+n_states = 40
+distance = (high_values - low_values) 
+step = distance / n_states
+
+
+def discretise(state):
+    """
+    This functions  assigns a discrete number  to a state 
+    """
+
+    state = (state - low_values) / step
+    return tuple(state.astype(int))
+
+
+
 
