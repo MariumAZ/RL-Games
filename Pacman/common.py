0cdae7ffb5a5273e17ce650c8baa2e190a5dcf0d
MariumAZ <mariem.azouz@dauphine.tn>
2021-11-05 15:50:41 +0100

    added utilities functions

--- Pacman/common.py
+++ Pacman/common.py
@@ -1,4 +1,24 @@
 #libraries 
+import cv2
+import numpy as np
+
+
+def preprocess(image):
+    #transform an RGB image into gray scale image
+    if image.shape[2] == 3:
+        #convert image to grey scale
+        grey_image = image.mean(axis=2)
+        #add dimension to image to match keras layers expectations
+        grey_image = grey_image[:,:,np.newaxis]
+    return grey_image
+
+
+def sample(n):
+    #sampling experiences from buffer :
+    #we can pass in a number or a pourcentage 
+    examples = n.random.randint()
+
+
 
 
 
--- Pacman/model.py
+++ Pacman/model.py
@@ -2,9 +2,14 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Dense, MaxPool2D, Dropout, AveragePooling2D
 from tensorflow.keras.models import Model
 
+buffer_size = 100 #will take sequence of tuples (s_t, a_t, r_t, s_t+1)
+number_episodes = 10000
 nb_actions = 4
 window_lenght = 4 #number of images we will consider in 
-target_input = (84, 84, 1)
+image_shape = (84, 84)
+target_input = image_shape + (window_lenght,)
+
+#TODO: try time distributed layer
 
 def model(nb_actions):
     input = Input(shape=target_input)
@@ -16,6 +21,20 @@ def model(nb_actions):
     output = Dense(nb_actions, activation='softmax')
     model = Model(inputs=input, outputs=output) 
     return model
+
+
+def clone_model(model):
+    """
+    The target network will need the same configuration as the 
+    policy network 
+    """
+    
+
+
+
+
+
+
 
 
 
