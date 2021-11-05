8020ff4622bcde4c4df157e989af76184b2ee3ba
MariumAZ <mariem.azouz@dauphine.tn>
2021-11-05 15:27:48 +0100

    added pacman game

--- Pacman/common.py
+++ Pacman/common.py
@@ -1,22 +1,4 @@
 #libraries 
-import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, Dense, MaxPool2D, Dropout, AveragePooling2D
-from tensorflow.keras.models import Model
-
-nb_actions = 4
-window_lenght = 4 #number of images we will consider in 
-target_input = (84, 84, 1)
-
-def model(nb_actions):
-    input = Input(shape=target_input)
-    x = input
-    for i in range(5):
-        x = Conv2D(2**(i + 5), activation="relu")(x)
-        x = MaxPool2D()(x)
-    x = AveragePooling2D()(x)
-    output = Dense(nb_actions, activation='softmax')
-    model = Model(inputs=input, outputs=output) 
-    return model
 
 
 
